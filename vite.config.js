eb8985ad8fd72ffaeda46798dbde96717683d8c8
spdevgroup1 <229469389+spdevgroup1@users.noreply.github.com>
2026-06-04 07:48:48 +1000

    Enhance Vite config with path aliasing
    Added path aliasing for easier imports.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/test-github/', // 👈 Updated with your repository name
+  base: '/test-github/',
   logLevel: 'error',
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // This tells Vite that whenever it sees "@/", it should look in the "src" folder
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
