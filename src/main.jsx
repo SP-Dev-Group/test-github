62ff08d1311e857ba35a10a4458b2a4f71830eb3
spdevgroup1 <229469389+spdevgroup1@users.noreply.github.com>
2026-06-04 16:03:45 +1000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './index.css'
 
 // 1. Import the necessary pieces from TanStack Query
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
